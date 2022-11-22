a0c3fbd2093b46e6d5f2df0b67ef0f089cec8e0a
Gabriel <gabriel02aguiar@gmail.com>
2022-11-21 22:56:37 -0300

    nodeCommit

---
+++ nodeJS/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(request, response) {
+    return response.json('ehagfqiuyr388y1784y38rbujks')
+})
+
+app.listen(3000)
+
+//GET http://localhost:3000/
+ //request traz todas as informações enviadas pela aplicação cliente
+ //response serve para formatar tudo que vai ser devolvido à aplicação cliente
