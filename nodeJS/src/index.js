d25f426c244870085d0222fe1efced697724bd43
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2022-11-22 20:45:12 -0300

    nodeCommit

--- nodeJS/src/index.js
+++ nodeJS/src/index.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(request, response) {
-    return response.json('ehagfqiuyr388y1784y38rbujks')
+app.get('/projects', function(request, response) {
+    return response.json([
+        'projeto 1',
+        'projeto 2'
+    ])
 })
 
-app.listen(3000)
+app.post('/projects', function(request, response) {
+    return response.json([
+        'projeto 1',
+        'projeto 2',
+        'projeto 3'
+    ])
+})
 
-//GET http://localhost:3000/
+app.put('/projects/:id', function(request, response) { //put = identificacao do projeto que quero atualizar
+    return response.json([
+        'projeto 4',
+        'projeto 2',
+        'projeto 3'
+    ])
+})
+
+app.delete('/projects/:id', function(request, response) { //put = identificacao do projeto que quero atualizar
+    return response.json([
+        'projeto 2',
+        'projeto 3'
+    ])
+})
+
+app.listen(5000, () => {
+    console.log('server started on port 5000')
+}) 
+
+//GET http://localhost:5000/projects
  //request traz todas as informações enviadas pela aplicação cliente
  //response serve para formatar tudo que vai ser devolvido à aplicação cliente
+ //() => {} arrow function é uma anotacao abreviada de function
