7967709f99277dd25c2ff65716635f02b8696547
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2022-12-03 09:37:33 -0300

    nodeCommit

---
+++ myApi/dist/server.js
@@ -0,0 +1,6 @@
+class app {
+    constructor(info) {
+        this.app = info !== null && info !== void 0 ? info : 'Olá Mundo'; //?? = caso não tenha nada dentro da variável info ou qualquer q tenha sido criada 
+    }
+}
+//# sourceMappingURL=server.js.map
