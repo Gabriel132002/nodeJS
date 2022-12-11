37c0e7ea909b4b4aa419dcc990805e0beb9a540f
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2022-12-11 00:24:27 -0300

    nodeCommit

--- myApi/src/shared/http/server.ts
+++ myApi/src/shared/http/server.ts
@@ -1,7 +1,7 @@
-class app {
-    private app: string
+export class AppServer {
+  private app: string
 
-    constructor(info: string) {
-        this.app = info ?? 'Olá Mundo' //?? = caso não tenha nada dentro da variável info ou qualquer q tenha sido criada 
-    }
+  constructor(info: string) {
+    this.app = info ?? 'Olá Mundo' //?? = caso não tenha nada dentro da variável info ou qualquer q tenha sido criada
+  }
 }
